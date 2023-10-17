9ef45b9b889ef791c271a026ef1da4fa8f6a30ea
Sumit Yadav <sumit2911997@gmail.com>
2023-10-17 17:59:54 +0530

    init

---
+++ src/main/java/com/devworks/sbootask/SbootaskApplication.java
@@ -0,0 +1,13 @@
+package com.devworks.sbootask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devworks/sbootask/SbootaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devworks.sbootask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
